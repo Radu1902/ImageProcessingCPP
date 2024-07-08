9d41c7b7c4d6d9418b61e1cddc0bd9aa5dfe6829
radu1902 <radu.pultea@gmail.com>
2024-07-09 01:21:44 +0300

    file dialog

--- ImageProcessingCPP.cpp
+++ ImageProcessingCPP.cpp
@@ -1,7 +1,31 @@
 #include "image_loader.h"
 #include "gui.h"
+#include "ImGuiFileDialog.h"
 
 
+const char* drawGui(bool* keep_open) {
+    // open Dialog Simple
+    IGFD::FileDialogConfig config;
+    config.path = ".";
+    ImGuiFileDialog::Instance()->OpenDialog("ChooseFileDlgKey", "Choose File", ".jpg,.png", config);
+    const char* path_string = nullptr;
+    // display
+    if (ImGuiFileDialog::Instance()->Display("ChooseFileDlgKey")) {
+        if (ImGuiFileDialog::Instance()->IsOk()) { // action if OK
+            std::string filePathName = ImGuiFileDialog::Instance()->GetFilePathName();
+            std::string filePath = ImGuiFileDialog::Instance()->GetCurrentPath();
+            // action
+            const char* path_string = filePathName.c_str();
+            // *keep_open = false;
+            ImGuiFileDialog::Instance()->Close();
+        }
+
+        // close
+        *keep_open = false;
+        ImGuiFileDialog::Instance()->Close();
+        return path_string;
+    }
+}
 
 int main(int, char**)
 {
@@ -29,11 +53,18 @@ int main(int, char**)
 
 
 
-    bool show_demo_window = false;
-    bool show_another_window = false;
+    bool show_demo_window = true;
 
-    bool show_imgproc = false;
-    bool show_menu = false;
+
+    const char* input_path = NULL;
+    bool show_path_chooser = false;
+
+    bool input_image_exists = false;
+    bool output_image_exists = false;
+    bool show_input_image = false;
+    bool show_output_image = false;
+
+    bool show_imgproc_operations = false;
 
     int my_image_width = 0;
     int my_image_height = 0;
@@ -53,58 +84,68 @@ int main(int, char**)
         if (show_demo_window)
             ImGui::ShowDemoWindow(&show_demo_window);
 
-        {
-            ImGui::Begin("Image Processing", &show_imgproc, ImGuiWindowFlags_MenuBar);
-            ImGui::Text("Hello");
-            if (ImGui::Button("Upload"))
-            {
-                show_menu = true;
-            }
-            if (show_menu)
-            {
-                if (ImGui::BeginMenuBar())
-                {
-                    
-                    ImGui::EndMenuBar();
-                }
-            }
-            
-            ImGui::End();
-        }
-
-        {
-            ImGui::Begin("OpenGL Texture Text");
-            ImGui::Text("pointer = %x", my_image_texture);
-            ImGui::Text("size = %d x %d", my_image_width, my_image_height);
-            ImGui::Image((void*)(intptr_t)my_image_texture, ImVec2(my_image_width, my_image_height));
-            ImGui::End();
-        }
         
 
-        if (show_menu)
+
+        
+
         {
             if (ImGui::BeginMainMenuBar())
             {
                 if (ImGui::BeginMenu("Main menu"))
                 {
+                    ImGui::MenuItem("Choose file", NULL, &show_path_chooser);
                     ImGui::EndMenu();
                 }
-                if (ImGui::BeginMenu("Pointwise operations"))
+                if (show_imgproc_operations)
                 {
-                    ImGui::EndMenu();
-                }
-                if (ImGui::BeginMenu("Thresholdings"))
-                {
-                    ImGui::EndMenu();
-                }
-                if (ImGui::BeginMenu("Filters"))
-                {
-                    ImGui::EndMenu();
+                    if (ImGui::BeginMenu("Pointwise operations"))
+                    {
+                        ImGui::EndMenu();
+                    }
+                    if (ImGui::BeginMenu("Thresholdings"))
+                    {
+                        ImGui::EndMenu();
+                    }
+                    if (ImGui::BeginMenu("Filters"))
+                    {
+                        ImGui::EndMenu();
+                    }
                 }
                 ImGui::EndMainMenuBar();
             }
         }
-        
+
+        if (show_path_chooser)
+        {
+            input_path = drawGui(&show_path_chooser);
+            printf("%s\n", input_path);
+        }
+
+        {
+            ImGui::Begin("Input Image");
+            ImGui::Text("pointer = %x", my_image_texture);
+            ImGui::Text("size = %d x %d", my_image_width, my_image_height);
+            if (input_path != nullptr)
+            {
+                ImGui::Text("file path: %s", *input_path);
+            }
+            if (show_input_image)
+            {
+                ImGui::Image((void*)(intptr_t)my_image_texture, ImVec2(my_image_width, my_image_height));
+            }
+            ImGui::End();
+        }
+        {
+            ImGui::Begin("Output Image");
+            ImGui::Text("pointer = %x", my_image_texture);
+            ImGui::Text("size = %d x %d", my_image_width, my_image_height);
+            if (show_output_image)
+            {
+                ImGui::Image((void*)(intptr_t)my_image_texture, ImVec2(my_image_width, my_image_height));
+            }
+            ImGui::End();
+        }
 
         // Rendering
         ImGui::Render();
